2b000d69f70419e09af1913efd2dbb1ef2ef3858
Alan Williams <alanwill81@gmail.com>
2013-07-11 04:59:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buffer = new Buffer(16);
+
+fs.readFile('index.html', "utf-8", function (err, data) {
+  if (err) throw err;
+  response.send(data.toString());
 });
+
+buffer.toString('utf-8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
