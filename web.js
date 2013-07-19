322d105094af43fe65c6915b2ba8c99421b8d4a5
Alan Williams <alanwill81@gmail.com>
2013-07-19 03:57:48 +0000

    Removed erroneous space

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new buffer ();
+	var fs = require('fs');
+	var buffer = new buffer();
   response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
  });
 
